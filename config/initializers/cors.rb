e98ccd1edd32831e28745d211adc6dd9cf42a3cb
Ben Benedict <Ben.Benedict@gmail.com>
2021-02-22 20:03:30 -0600

    update cors to allow netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:8080", "mini-capstone.netlify.app"
+    origins "localhost:8080", "mini-capstone.netlify.app", "https://stoic-allen-22a479.netlify.app/"
 
     resource "*",
       headers: :any,
